841658f0af4115bb510694733f61b055bd89eb44
Isak La Fleur <isak.engdahl@gmail.com>
2017-05-05 11:52:49 +0200

    added mongodb database connection

---
+++ configs/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const dbName = 'project2-experify';
+const port = '27017';
+
+// connect to the database
+mongoose.connect(`mongodb://localhost${port}/${dbName}`);
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {  
+  console.log(`Connected to the ${dbName} database`);
+});
