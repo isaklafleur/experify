08962f20e25416da8e6c965facbdee61e06bf285
Isak La Fleur <isak.engdahl@gmail.com>
2017-05-05 13:09:19 +0200

    fixed the mongoose models

--- models/user.js
+++ models/user.js
@@ -4,10 +4,10 @@ const UserSchema = mongoose.Schema({
   name: String,
   email: String,
   password: String,
-  experiences: [{ type: Schema.Types.ObjectId, ref: 'Experience' }],
+  experiences: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Experience' }],
 },
-{
-  timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
-});
+  {
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
+  });
 
-module.exports = mongoose.model('Host', HostSchema);
+module.exports = mongoose.model('User', UserSchema);
